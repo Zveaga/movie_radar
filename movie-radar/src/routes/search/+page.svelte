64d112db36739b544346a6706d63cee18246bef8
wsl <wsl-h>
2025-09-22 20:38:19 +0200

    fix: update search results when query changes without page reload

--- movie-radar/src/routes/search/+page.svelte
+++ movie-radar/src/routes/search/+page.svelte
@@ -2,19 +2,33 @@
 	import MovieCard from "$lib/MovieCard.svelte";
 	import { page } from "$app/stores";
 	import { onMount } from "svelte";
+	import { browser } from "$app/environment";
+
 
 	let searchResults: any[] = [];
 	let query = '';
 
 	$: query = $page.url.searchParams.get('q') ?? '';
 
-	onMount(async () => {
-		if (query) {
-			const res = await fetch(`/api/search?q=${encodeURIComponent(query)}`);
-			searchResults = await res.json();
-			console.log('Results:', searchResults);
-		}
-	});
+	// onMount(() => {
+    //     if (query) {
+    //         fetchResults(query);
+    //     } else {
+    //         searchResults = [];
+    //     }
+    // });
+
+	$: if (browser && query) {
+        fetchResults(query);
+    } else if (browser && !query) {
+        searchResults = [];
+    }
+
+    async function fetchResults(q: string) {
+        const res = await fetch(`/api/search?q=${encodeURIComponent(q)}`);
+        searchResults = await res.json();
+        console.log('Results:', searchResults);
+    }
 </script>
 
 
